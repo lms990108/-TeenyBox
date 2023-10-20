825a327f41e9c53d3d8cf152908dc64436abf5e2
donghyun123 <numnum97@naver.com>
2023-10-21 03:08:41 +0900

    First commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = 5001;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
