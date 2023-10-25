3d8b34171028d9d93f7dfa65126f14509ee13b7d
lee minseop <lms990108@gmail.com>
2023-10-25 21:28:10 +0900

    Feat: MongoDB 연결

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 5001;
+const mongoURI = process.env.MONGO_DB_PATH;
+
+mongoose
+  .connect(mongoURI as string)
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.error("DB connection fail", err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
