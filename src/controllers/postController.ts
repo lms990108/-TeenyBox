7c26f60901601105f8e271bc0380544f1713030a
lee minseop <lms990108@gmail.com>
2023-10-30 22:23:59 +0900

    Refactor: 파라미터 postNumber(카멜 케이스)로 통일

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -8,7 +8,7 @@ class PostController {
   }
 
   async updatePost(req: Request, res: Response): Promise<void> {
-    const postNumber = Number(req.params.post_number);
+    const postNumber = Number(req.params.postNumber);
     const post = await PostService.update(postNumber, req.body);
     res.status(200).json(post);
   }
