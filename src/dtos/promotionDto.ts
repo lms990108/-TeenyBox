a5455132e8a5aab4b8af0499a27e489e7d711f4c
lee minseop <lms990108@gmail.com>
2023-10-30 23:32:02 +0900

    Feat: DTO에 에러메세지 추가

--- src/dtos/promotionDto.ts
+++ src/dtos/promotionDto.ts
@@ -5,37 +5,37 @@ import { IsNotEmpty, IsString, IsInt, MaxLength } from "class-validator";
 export class CreatePromotionDTO {
   promotion_number?: number;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: "user_id는 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "user_id는 반드시 입력되어야 합니다." })
   user_id!: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(30)
+  @IsString({ message: "제목은 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "제목은 반드시 입력되어야 합니다." })
+  @MaxLength(30, { message: "제목은 30자를 초과할 수 없습니다." })
   title!: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: "내용은 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "내용은 반드시 입력되어야 합니다." })
   content!: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: "포스터 사진 url은 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "포스터 사진은 반드시 입력되어야 합니다." })
   poster_url!: string;
 }
 
 // 게시글 수정을 위한 DTO
 export class UpdatePromotionDTO {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(30)
+  @IsString({ message: "제목은 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "제목은 반드시 입력되어야 합니다." })
+  @MaxLength(30, { message: "제목은 30자를 초과할 수 없습니다." })
   title!: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: "내용은 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "내용은 반드시 입력되어야 합니다." })
   content!: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: "포스터 사진 url은 문자열이어야 합니다." })
+  @IsNotEmpty({ message: "포스터 사진은 반드시 입력되어야 합니다." })
   poster_url!: string;
 }
 // ... 나머지 DTO들도 필요에 따라 추가적으로 정의
