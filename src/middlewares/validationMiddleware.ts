d60472f7959e5113767f09bdcfc558160dcf5c20
lee minseop <lms990108@gmail.com>
2023-10-30 23:44:07 +0900

    Feat: validation 에서 에러 처리 추가.

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -8,10 +8,44 @@ export function validationMiddleware(type: any): RequestHandler {
 
     validate(dto).then((errors: ValidationError[]) => {
       if (errors.length > 0) {
-        const message = errors
-          .map((error) => Object.values(error.constraints!))
-          .join(", ");
-        res.status(400).json({ message });
+        const errorResponses = errors.map((error) => {
+          let errorCode;
+          let errorMessage;
+
+          switch (error.property) {
+            case "user_id":
+              errorCode = 422;
+              errorMessage = Object.values(error.constraints!).join(", ");
+              break;
+            case "title":
+              errorCode = 423;
+              errorMessage = Object.values(error.constraints!).join(", ");
+              break;
+            case "content":
+              errorCode = 424;
+              errorMessage = Object.values(error.constraints!).join(", ");
+              break;
+            case "poster_url":
+              errorCode = 425;
+              errorMessage = Object.values(error.constraints!).join(", ");
+              break;
+            default:
+              errorCode = 400; // 기본값: Bad Request
+              errorMessage = Object.values(error.constraints!).join(", ");
+          }
+
+          return {
+            field: error.property,
+            code: errorCode,
+            message: errorMessage,
+          };
+        });
+
+        // 가장 처음 발생한 에러의 상태 코드를 반환합니다.
+        // 또는 여러 에러를 한번에 반환하려면 상태 코드를 조정해야 합니다.
+        res.status(errorResponses[0].code).json({
+          errors: errorResponses,
+        });
       } else {
         req.body = dto;
         next();
