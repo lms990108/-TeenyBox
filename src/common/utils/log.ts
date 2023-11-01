84894eff7fb6a0fbb4e9438b50a24f635b63ee37
donghyun123 <numnum97@naver.com>
2023-11-01 22:45:04 +0900

    Fix: log 에러 fix 및 multer-s3 라이브러리 버전 3->2로 다운그레이드

--- src/common/utils/log.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "../../log/access.log"),
-  { flags: "a" },
-);
-
-export default accessLogStream;
