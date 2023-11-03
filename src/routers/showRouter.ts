daa5e5824d65fe67b7ebc4b645b3490dcf9ab0ea
donghyun123 <numnum97@naver.com>
2023-11-04 00:00:17 +0900

    Feat: 공연 기능 리팩토링 및 에러 핸들링 추가

--- src/routers/showRouter.ts
+++ src/routers/showRouter.ts
@@ -7,9 +7,7 @@ const showRouter = Router();
 showRouter.get("/", asyncHandler(ShowController.findShows));
 showRouter.get("/:showId", asyncHandler(ShowController.findShowByShowId));
 showRouter.get("/title/:title", asyncHandler(ShowController.findShowByTitle));
-showRouter.get("/search/title", asyncHandler(ShowController.searchByTitle));
-showRouter.get("/search/status", asyncHandler(ShowController.searchByStatus));
-showRouter.get("/search/region", asyncHandler(ShowController.searchByRegion));
+showRouter.get("/search", asyncHandler(ShowController.search));
 showRouter.delete("/:showId", asyncHandler(ShowController.deleteByShowId));
 
 export default showRouter;
