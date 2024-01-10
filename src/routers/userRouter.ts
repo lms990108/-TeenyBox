93a5f3cf7a015d6fa6fa77164d7d379c87b598c5
eunli <eunli99012@gmail.com>
2024-01-11 01:50:35 +0900

    Feat: 찜 관련 기능 스웨거 추가 #34

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -722,6 +722,155 @@ const router = express.Router();
  */
 /**
  * @swagger
+ * /user/bookmarks:
+ *   get:
+ *     tags: [user]
+ *     summary: 유저 찜 목록 조회
+ *     parameters:
+ *      - in: query
+ *        name: page
+ *        schema:
+ *         type: number
+ *         description: 페이지 번호
+ *      - in: query
+ *        name: limit
+ *        schema:
+ *         type: number
+ *         description: 페이지당 공연 개수
+ *     responses:
+ *       200:
+ *         description: 찜 목록 조회 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 bookmarks:
+ *                   type: array
+ *                   example:
+ *                     - _id: "65490f47efc7d02dc13d0168"
+ *                       title: "R.U.R 인류를 삭제하다"
+ *                       poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF229380_231106_134408.jpg"
+ *                       region: "경기/인천"
+ *                       company: "이리떼고찰소"
+ *                     - _id: "658a60e2b22d61d2e4270dd3"
+ *                       title: "7층의 섬"
+ *                       poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF232481_231214_093851.gif"
+ *                       region: "서울"
+ *                       company: ""
+ *                     - _id: "65490f47efc7d02dc13d0126"
+ *                       title: "2호선 세입자 [대구]"
+ *                       poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF228269_231020_141133.gif"
+ *                       region: "대구"
+ *                       company: ""
+ *                     - _id: "65490f47efc7d02dc13d0114"
+ *                       title: "12인의 성난 사람들 [부산]"
+ *                       poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF228558_231025_100855.jpg"
+ *                       region: "부산"
+ *                       company: ""
+ *                     - _id: "659c2a242df50c77b22d146e"
+ *                       title: "1 HOUR"
+ *                       poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF233557_240108_130948.jpg"
+ *                       region: "서울"
+ *                       company: ""
+ *                     - _id: "65490f52efc7d02dc13d0185"
+ *                       title: "(당)신의 재판 [창원]"
+ *                       poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF229206_231102_135513.jpg"
+ *                       region: "경상"
+ *                       company: ""
+ *
+ */
+/**
+ * @swagger
+ * /user/is-bookmarked/{showId}:
+ *   get:
+ *     tags: [user]
+ *     summary: 찜 여부 확인
+ *     responses:
+ *       200:
+ *         description: 찜 true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 isBookmarked:
+ *                   type: boolean
+ *                   description: Description of the property
+ *                   example: true
+ */
+/**
+ * @swagger
+ * /user/save/{showId}:
+ *   put:
+ *     tags: [user]
+ *     summary: 찜 추가(공연 상세 페이지)
+ *     responses:
+ *       200:
+ *         description: 찜 추가 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Description of the property
+ *                   example: "공연 찜이 완료되었습니다."
+ */
+/**
+ * @swagger
+ * /user/cancel/{showId}:
+ *   put:
+ *     tags: [user]
+ *     summary: 찜 취소(공연 상세 페이지)
+ *     responses:
+ *       200:
+ *         description: 찜 취소 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Description of the property
+ *                   example: "공연 찜이 취소되었습니다."
+ */
+/**
+ * @swagger
+ * /user/cancels/bookmarks:
+ *   put:
+ *     tags: [user]
+ *     summary: 찜 취소(마이페이지 유저 찜 목록)
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               showIds:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 example:
+ *                   - "65490f47efc7d02dc13d0168"
+ *                   - "658a60e2b22d61d2e4270dd3"
+ *     responses:
+ *       200:
+ *         description: 찜 취소 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Description of the property
+ *                   example: "찜한 공연이 삭제되었습니다."
+ */
+/**
+ * @swagger
  * /user/admin/users:
  *   get:
  *     tags: [user]
