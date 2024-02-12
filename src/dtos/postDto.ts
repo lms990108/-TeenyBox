96b7b89ab278e06fafb5536e99c730e675b6c135
lee minseop <lms990108@gmail.com>
2024-02-12 20:33:37 +0900

    Fix: 제목 길이제한 40자로 변경

--- src/dtos/postDto.ts
+++ src/dtos/postDto.ts
@@ -6,7 +6,7 @@ export class CreatePostDTO {
 
   @IsString({ message: "제목은 문자열이어야 합니다." })
   @IsNotEmpty({ message: "제목은 반드시 입력되어야 합니다." })
-  @MaxLength(30, { message: "제목은 30자를 초과할 수 없습니다." })
+  @MaxLength(40, { message: "제목은 40자를 초과할 수 없습니다." })
   title!: string;
 
   @IsString({ message: "내용은 문자열이어야 합니다." })
@@ -21,7 +21,7 @@ export class CreatePostDTO {
 export class UpdatePostDTO {
   @IsString({ message: "제목은 문자열이어야 합니다." })
   @IsNotEmpty({ message: "제목은 반드시 입력되어야 합니다." })
-  @MaxLength(30, { message: "제목은 30자를 초과할 수 없습니다." })
+  @MaxLength(40, { message: "제목은 40자를 초과할 수 없습니다." })
   title!: string;
 
   @IsString({ message: "내용은 문자열이어야 합니다." })
